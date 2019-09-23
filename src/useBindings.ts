53904d09e939a13ed85da99a04637bde76e6f795
Adrian Gallardo <adrian.gallardo@wizeline.com>
2019-09-23 14:07:17 -0500

    feat: web-components and use-bindings

---
+++ src/useBindings.ts
@@ -0,0 +1,60 @@
+import { useLayoutEffect, useRef, MutableRefObject } from 'react'
+
+type Maybe<T> = T | null
+
+const domNodes = new Map<HTMLElement, Map<string, EventListenerOrEventListenerObject>>()
+
+function useBindings(bindings: { [key: string]: any }) {
+  const ref: MutableRefObject<any> = useRef(null)
+
+  useLayoutEffect(() => {
+    const domNode: Maybe<HTMLElement> = ref.current
+
+    if (!domNode) {
+      return
+    }
+
+    for (let prop in bindings) {
+      if (prop.startsWith('on')) {
+        const eventName = prop.replace(/on(\w)/i, (_, char) => char.toLowerCase())
+
+        bindEvent(domNode, eventName, bindings[prop])
+      } else {
+        // @ts-ignore
+        domNode[prop] = bindings[prop]
+      }
+    }
+    /**
+     * On destroy, we need to remove the reference to the domNode to avoid memory-leaks
+     */
+    return () => {
+      domNodes.delete(domNode)
+    }
+  }, [ref, bindings]);
+
+  return ref
+}
+
+function bindEvent(domNode: HTMLElement, eventName: string, eventHandler: EventListenerOrEventListenerObject) {
+  if (!domNodes.has(domNode)) {
+    domNodes.set(domNode, new Map())
+  }
+  const events = domNodes.get(domNode)
+
+  if (!events) {
+    return
+  }
+
+  const prevHandler = events.get(eventName)
+  if (prevHandler) {
+    domNode.removeEventListener(eventName, prevHandler)
+  }
+  if (typeof eventHandler === 'function') {
+    domNode.addEventListener(eventName, eventHandler)
+    events.set(eventName, eventHandler)
+  } else {
+    events.delete(eventName)
+  }
+}
+
+export default useBindings
---
+++ src/webComponents.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import useBindings from './useBindings'
+
+type Bindings = { [key: string]: any }
+
+type WebComponent = Object & { [key: string]: any }
+
+const webComponentNames: { [key: string]: string } = {}
+
+/**
+ * Proxy that will return the function to create web-component instances along with bindings
+ * @param namespace (Optional) If provided, component names will be prefixed with `{namespace}-`
+ */
+function createProxy(namespace?: string) {
+  return new Proxy<WebComponent>({}, {
+    get(_, componentName: string) {
+      return (bindings: Bindings) => {
+        const Component = getWebComponentName(componentName, namespace)
+        const ref = useBindings(bindings)
+console.log(componentName, { bindings })
+        // @ts-ignore
+        return <Component ref={ref} />
+      }
+    }
+  })
+}
+
+function getWebComponentName(camelCasedName: string, namespace?: string): string {
+  if (webComponentNames[camelCasedName]) {
+    return webComponentNames[camelCasedName]
+  }
+  const prefix = namespace ? namespace + '-' : ''
+  const webComponentName = prefix + camelCasedName.replace(/[A-Z]/g, (char, index) => (
+    `${index > 0 ? '-' : ''}${char.toLowerCase()}`
+  ))
+  webComponentNames[camelCasedName] = webComponentName
+
+  return webComponentName
+}
+
+function createNamespace(namespace: string): WebComponent {
+  return createProxy(namespace)
+}
+
+export default createProxy()
+export {
+  createNamespace
+}
